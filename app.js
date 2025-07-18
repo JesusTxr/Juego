4aa1ab1655fe1176d73001c54f307b55b76967a8
JesusTxr <chuchito27tm@gmail.com>
2025-07-18 12:03:32 -0600

    Cambios

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ import petController from './controllers/petController.js';
 import adoptionController from './controllers/adoptionController.js';
 import userController from './controllers/userController.js';
 import { swaggerUiServe, swaggerUiSetup } from './swagger.js';
+import cors from 'cors';
 
 mongoose.connect(process.env.MONGODB_URI)
+
 .then(() => console.log('Conectado a MongoDB Atlas'))
 .catch(err => console.error('Error de conexión a MongoDB:', err));
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
 app.use('/api', heroController)
 app.use('/api', petController)
@@ -28,7 +30,7 @@ if (!process.env.JWT_SECRET) {
   console.warn('ADVERTENCIA: JWT_SECRET no está definida en el archivo .env. Por favor, agrégala para la autenticación JWT.');
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, _ => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
     console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}/api-docs/swagger`);
